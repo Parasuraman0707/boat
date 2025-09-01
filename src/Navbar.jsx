e6f66827c07579f5f32f42f2247ddc0b0ad4f847
sakthibalasamivel <sakthibalasamivel2@gmail.com>
2025-09-01 18:54:37 +0530

    fgh

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -12,6 +12,8 @@ const Navbar = () => {
       <img className="begining" src={img1} alt="logo" />
       <div className="underline1">Parasuraman</div>
       <div className="underline2">SAKTHIBALA</div>
+      {/* <div className="underline3">corporate orders</div>
+      <div className="underline">gifting store</div> */}
       <div className="underline3">corporate orders</div>
       <div className="underline">gifting store</div>
       <select name="more" id="" className="moree">
